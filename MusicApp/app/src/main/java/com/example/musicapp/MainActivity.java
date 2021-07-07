012dde7d66af87eb7a48a52c08fd091647060d02
Mehtabkazmi <mehtabkazmi5@gmail.com>
2021-07-07 15:53:11 +0500

    Music App in Android with java

---
+++ MusicApp/app/src/main/java/com/example/musicapp/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.musicapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+
+public class MainActivity extends AppCompatActivity {
+    private Button play, pause;
+    private SeekBar seekBar;
+    MediaPlayer mediaPlayer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        play=findViewById(R.id.play);
+        pause=findViewById(R.id.pause);
+
+        mediaPlayer=MediaPlayer.create(this,R.raw.kam_na_hoga);
+        mediaPlayer.start();
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.start();
+            }
+        });
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.pause();
+            }
+        });
+    }
+}
